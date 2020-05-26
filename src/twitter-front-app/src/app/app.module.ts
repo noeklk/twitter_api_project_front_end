01ed81b05f7f2e6ce8c69c1c1229e00dd0b2f3ea
SelmaniZayd <selmani.zayd@gmail.com>
2020-05-26 09:10:02 +0200

    initaliazing bar-chart template

--- src/twitter-front-app/src/app/app.module.ts
+++ src/twitter-front-app/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './component/home/home.component';
 import { NavComponent } from './component/nav/nav.component';
 import { SignupFormComponent } from './component/signup-form/signup-form.component';
+import { BarChartComponent } from './template/bar-chart/bar-chart.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { SignupFormComponent } from './component/signup-form/signup-form.compone
     LoginFormComponent,
     HomeComponent,
     NavComponent,
-    SignupFormComponent
+    SignupFormComponent,
+    BarChartComponent
   ],
   imports: [
     BrowserModule,
