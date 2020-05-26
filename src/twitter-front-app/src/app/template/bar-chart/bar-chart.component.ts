8f77f7b1327b06d64c6cb9db1169787be0cc3f9e
SelmaniZayd <selmani.zayd@gmail.com>
2020-05-26 09:24:50 +0200

    implémentation of the bar-chart template

--- src/twitter-front-app/src/app/template/bar-chart/bar-chart.component.ts
+++ src/twitter-front-app/src/app/template/bar-chart/bar-chart.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {
+  IBarChartOptions,
+  IChartistAnimationOptions,
+  IChartistData
+} from 'chartist';
+import { ChartEvent, ChartType } from 'ng-chartist';
 
 @Component({
   selector: 'app-bar-chart',
@@ -7,9 +13,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarChartComponent implements OnInit {
 
+  // Les données passé par le parent
+  @Input() labels = [];
+  @Input() series = [];
+
+  // Initialisation des varibles pour le bar-chart
+  type: ChartType;
+  data: IChartistData;
+  options: IBarChartOptions;
+  events: ChartEvent;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.type = 'Bar';
+
+    this.data = {
+      labels: this.labels,
+      series: this.series
+    };
+
+    this.options = {
+      axisX: {
+        showGrid: false
+      },
+      height: 300
+    };
+
+    this.events = {
+      draw: (data) => {
+        if (data.type === 'bar') {
+          data.element.animate({
+            y2: <IChartistAnimationOptions>{
+              dur: '0.5s',
+              from: data.y1,
+              to: data.y2,
+              easing: 'easeOutQuad'
+            }
+          });
+        }
+      }
+    };
   }
 
 }
