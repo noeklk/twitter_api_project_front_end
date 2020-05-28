8035205c4fc90c146b2e36a2bcde12f4d5ffe5f7
SelmaniZayd <selmani.zayd@gmail.com>
2020-05-28 21:28:08 +0200

    design fait mais bug sur le routing à fixer plus tard

--- src/twitter-front-app/src/app/component/tweets/tweets.component.ts
+++ src/twitter-front-app/src/app/component/tweets/tweets.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TwitterService } from 'src/app/service/twitter.service';
 import { TweetModel } from 'src/app/model/tweet';
+import { SessionService } from 'src/app/service/session.service';
 
 @Component({
   selector: 'app-tweets',
@@ -12,11 +13,14 @@ export class TweetsComponent implements OnInit {
   tweets: any;
 
   constructor(
-    private twitterService: TwitterService
+    private twitterService: TwitterService,
+    private sessionService: SessionService
   ) { }
 
   ngOnInit(): void {
-    this.GetUserTweets();
+    if (this.sessionService.CheckAccessTokens()) {
+      this.GetUserTweets();
+    }
   }
 
   GetUserTweets() {
--- src/twitter-front-app/src/app/service/auth.service.ts
+++ src/twitter-front-app/src/app/service/auth.service.ts
@@ -6,6 +6,7 @@ import { environment } from 'src/environments/environment';
 import { UserDto } from '../dto/user';
 import { LoginResponseModel } from '../model/login-response';
 import { TwitterService } from './twitter.service';
+import { SessionService } from './session.service';
 
 @Injectable({
     providedIn: 'root'
@@ -21,7 +22,11 @@ export class AuthService {
 
     public isLoggedIn = false;
 
-    constructor(private myRoute: Router, private http: HttpClient, private twitterService: TwitterService) { }
+    constructor(
+        private myRoute: Router,
+        private http: HttpClient,
+        private twitterService: TwitterService,
+        private sessionService: SessionService) { }
 
     SetUserToken(token: string) {
         localStorage.setItem(this._tokenName, token);
@@ -77,18 +82,19 @@ export class AuthService {
         return res;
     }
 
-    Logout() {
+    async Logout() {
+        //invalidating the token only if he is logged
+        if (this.sessionService.CheckAccessTokens()) {
+            await this.twitterService.invalidateToken().subscribe(
+                res => console.log(res),
+                err => console.log(err)
+            );
+        }
         localStorage.removeItem(this._tokenName);
         localStorage.removeItem(this._userIdName);
         localStorage.removeItem('oauthAccessToken');
         localStorage.removeItem('oauthAccessTokenSecret');
         this.isLoggedIn = false;
-
-        //invalidating the token
-        this.twitterService.invalidateToken().subscribe(
-            res => console.log("token invalidé"),
-            err => console.log(err)
-        );
 
         console.log('Déconnecté!');
         this.myRoute.navigate(['login']);
